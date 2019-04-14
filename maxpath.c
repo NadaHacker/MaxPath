7d2f84d17ad424ceca1a43a1b6cb765390955ea1
jh642 <jh642@evansville.edu>
2019-04-13 22:54:16 -0500

    finished project

--- maxpath.c
+++ maxpath.c
@@ -12,19 +12,35 @@ maxpath.c
 #include <stdio.h>
 
 int maxPathTotal(int depth, int* triangle) {
-  int size;
-  
-  //int size = sizeof(triangle)/sizeof(triangle[0]);
-  printf("last leaf at index %d\n", size);
-  int firstLeaf = size - depth;
-  return firstLeaf;
+  int size = (depth*(depth+1))/2;
+  int level = depth;
+  int lastLeaf = (size-1) - depth;
+  int counter = 0;
+  int rChild;
+  int lChild;
+  for (int i = lastLeaf; i > -1; i--){
+     rChild = i+level;
+     lChild = i+(level-1);
+     if (triangle[rChild] > triangle[lChild]) {
+	triangle[i] = triangle[i] + triangle[rChild];
+     }
+     else {
+	triangle[i] = triangle[i] + triangle[lChild];
+     }
+     counter++;
+     if (counter == (level-1)){
+	level = level - 1;
+	counter = 0;
+     }
+  }
+  return triangle[0];
 }
 
 // for testing purposes only
-int main()
-{
-  int A[] = {3, 7, 4, 2, 4, 6, 8, 5, 9, 3};
-  int leaf = maxPathTotal(4, A);
-  printf("last leaf at index %d\n", leaf);
-  return 0;
-}
+/* int main() */
+/* { */
+/*   int A[] = {3, 7, 4, 2, 4, 6, 8, 5, 9, 3}; */
+/*   int leaf = maxPathTotal(4, A); */
+/*   printf("maxPath: %d\n", leaf); */
+/*   return 0; */
+/* } */
